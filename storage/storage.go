b37991368ff17c66dc6e0b41f4274246cb8aeab3
Eugen Bondarev <eug.bondarev@gmail.com>
2025-05-08 18:38:16 +0200

    storage.go: New(cfg) returns a storage implementation based on config

--- storage/storage.go
+++ storage/storage.go
@@ -1,5 +1,26 @@
 package storage
 
+import (
+	"errors"
+	"log"
+
+	"github.com/eugen-bondarev/backup-tool/config"
+)
+
 type Storage interface {
 	Push(localFileName, remoteFileName string) error
+}
+
+func New(cfg config.Config) (Storage, error) {
+	if cfg.GCPConfig.IsDefined() {
+		log.Printf("storage: using gcp backend")
+		return NewGCPStorage(cfg.GCPConfig)
+	}
+
+	if cfg.S3Config.IsDefined() {
+		log.Printf("storage: using s3 backend")
+		return NewS3Storage(cfg.S3Config)
+	}
+
+	return nil, errors.New("no storage config")
 }
