0c9138daf23f03eac30a6b7fa8e44dfb1096bbef
Eugen Bondarev <eug.bondarev@gmail.com>
2025-05-08 18:37:04 +0200

    SanitizeEndpoint helper function

--- util/buckets.go
+++ util/buckets.go
@@ -2,6 +2,10 @@ package util
 
 import "strings"
 
+func SanitizeEndpoint(endpoint string) string {
+	return strings.ReplaceAll(strings.ReplaceAll(endpoint, "http://", ""), "https://", "")
+}
+
 func GetBucketName(path string) (string, string) {
 	parts := strings.Split(path, "/")
 	return parts[0], strings.Join(parts[1:], "/")
--- util/buckets_test.go
+++ util/buckets_test.go
@@ -23,3 +23,17 @@ func TestGetBucketName(t *testing.T) {
 		t.Errorf("expected bar/baz/test.txt, got %s", path)
 	}
 }
+
+func TestSanitizeEndpoint(t *testing.T) {
+	endpoint := "https://s3.amazonaws.com"
+	sanitized := SanitizeEndpoint(endpoint)
+	if sanitized != "s3.amazonaws.com" {
+		t.Errorf("expected s3.amazonaws.com, got %s", sanitized)
+	}
+
+	endpoint = "http://s3.amazonaws.com"
+	sanitized = SanitizeEndpoint(endpoint)
+	if sanitized != "s3.amazonaws.com" {
+		t.Errorf("expected s3.amazonaws.com, got %s", sanitized)
+	}
+}
