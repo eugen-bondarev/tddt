c62ebdfb14da3b8bf0e98dfd2364e25fe1f7be03
Eugen Bondarev <eug.bondarev@gmail.com>
2026-06-26 07:00:12 +0200

    Bug fixes

--- config/pg.go
+++ config/pg.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type PGConfig struct {
 	Host     string `env:"HOST" envDefault:"127.0.0.1"`
+	Port     string `env:"PORT" envDefault:"5432"`
 	User     string `env:"USER" envDefault:"postgres"`
 	Password string `env:"PASSWORD"`
 }
@@ -13,5 +14,8 @@ type WithPGConfig struct {
 }
 
 func (c PGConfig) GetCommand(database, outputFile string) string {
-	return fmt.Sprintf("PGPASSWORD=%s pg_dump -U %s -h %s -d %s >> %s", c.Password, c.User, c.Host, database, outputFile)
+	return fmt.Sprintf(
+		"PGPASSWORD=%s pg_dump -U %s -h %s -p %s -d %s --no-password --file %s",
+		c.Password, c.User, c.Host, c.Port, database, outputFile,
+	)
 }
--- dump/cmd.go
+++ dump/cmd.go
@@ -1,12 +1,19 @@
 package dump
 
 import (
+	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func executeCommand(command string) error {
 	c := exec.Command("bash", "-c", command)
-	return c.Run()
+	var stderr strings.Builder
+	c.Stderr = &stderr
+	if err := c.Run(); err != nil {
+		return fmt.Errorf("%w: %s", err, strings.TrimSpace(stderr.String()))
+	}
+	return nil
 }
 
 type GetDumpCommand interface {
